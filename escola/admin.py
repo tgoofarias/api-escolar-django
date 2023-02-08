c9678ff953814a5248d0ece55df74a24d28776e1
Tiago Farias Barbosa <tfb.snf21@uea.edu.br>
2023-02-07 23:25:38 -0300

    Models serializers

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from escola.models import Aluno, Curso
 
 # Register your models here.
+class AlunoModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_per_page = 20
+
+admin.site.register(Aluno, AlunoModelAdmin)
+
+class CursoModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'codigo_curso', 'descricao')
+    list_display_links = ('id', 'codigo_curso')
+    search_fields = ('codigo_curso',)
+    list_per_page = 20
+
+admin.site.register(Curso, CursoModelAdmin)
--- escola/models.py
+++ escola/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class AlunoModel(models.Model):
+class Aluno(models.Model):
     nome = models.CharField(max_length=100)
     rg = models.CharField(max_length=9)
     cpf = models.CharField(max_length=11)
@@ -10,7 +10,7 @@ class AlunoModel(models.Model):
     def __str__(self):
         return self.nome
 
-class CursoModel(models.Model):
+class Curso(models.Model):
     NIVEL = (
         ('B', 'Básico'),
         ('I', 'Intermediário'),
